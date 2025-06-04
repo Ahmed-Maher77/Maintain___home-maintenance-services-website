7dcd0c3bbd3106314fef6a19ff0437cdd9073cfc
Ahmed Maher Algohary <ahmedmaheraljwhry057@gmail.com>
2025-06-05 00:12:22 +0300

    Add SEO-enhancing meta tags for improved search engine visibility and social sharing

--- js/custom.js
+++ js/custom.js
@@ -35,7 +35,7 @@ $(".owl-carousel").owlCarousel({
 
 function myMap() {
 	var mapProp = {
-		center: new google.maps.LatLng(40.712775, -74.005973),
+		center: new google.maps.LatLng(30.0444, 31.2357), // Cairo, Egypt
 		zoom: 18,
 	};
 	var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
